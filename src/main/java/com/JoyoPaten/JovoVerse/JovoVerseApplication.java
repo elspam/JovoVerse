0ccb205f58ddd67b98d6ebb6e2b7ff913cc1e4f3
elspam <kicaumerdu1@gmail.com>
2025-05-22 22:52:07 +0700

    first commit

---
+++ src/main/java/com/JoyoPaten/JovoVerse/JovoVerseApplication.java
@@ -0,0 +1,13 @@
+package com.JoyoPaten.JovoVerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JovoVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JovoVerseApplication.class, args);
+	}
+
+}

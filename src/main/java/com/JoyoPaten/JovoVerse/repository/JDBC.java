c0a649dd2b0f2ce0de7f0b1a8110699fdd697b64
Nurendra-bagaskara <bagaskara636@gmail.com>
2025-05-31 23:30:21 +0700

    integrasiDB

---
+++ src/main/java/com/JoyoPaten/JovoVerse/repository/JDBC.java
@@ -0,0 +1,20 @@
+package com.JoyoPaten.JovoVerse.repository;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+public class JDBC {
+    private static final String URL = "jdbc:mysql://localhost:3307/joyo_verse?serverTimezone=UTC&useSSL=false";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
